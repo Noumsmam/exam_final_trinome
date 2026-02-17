62f2da9292cb4b7100e3494e1512add88cf691a4
jojota69 <rktbjrhs@gmail.com>
2026-02-17 07:05:41 +0300

    Paufine fiche-besoin

--- app/views/pages/fiche-besoins.php
+++ app/views/pages/fiche-besoins.php
@@ -16,18 +16,6 @@
             <h1><?php echo $fiche['nom_ville']; ?></h1>
         </div>
 
-        <div class="creer-besoin">
-            <form action="#" method="post">
-                <select name="besoin" id="besoin">
-                    <!-- boucle -->
-                    <option value="0">Selectionner</option>
-                    <?php  foreach($listeArticle as $row) { ?>
-                        <option value="<?php echo $row['id_article']; ?>"><?php echo $row['nom_article']; ?></option>
-                    <?php } ?>
-                </select>
-                <input type="submit" value="Valider">
-            </form>
-        </div>
 
             <div class="creer-besoin card" style="margin-top:12px;">
                 <form action="#" method="post" style="display:flex;gap:8px;align-items:center;">
